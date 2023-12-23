466d5a2ac53659a7bd1a822e589259d8486df852
thuctran <tranduythuc2003@gmail.com>
2023-12-23 22:39:41 +0700

    update post fb in groups

--- main.py
+++ main.py
@@ -7,23 +7,33 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+username = 'tranduythuc2003@gmail.com'
+password = '23070603'
+image_list = ["D:\\Code\\python\\toolAutoFb\\rs_image\\1.jpg", "D:\\Code\\python\\toolAutoFb\\rs_image\\2.jpg", "D:\\Code\\python\\toolAutoFb\\rs_image\\3.jpg", "D:\\Code\\python\\toolAutoFb\\rs_image\\4.jpg"
+              "D:\\Code\\python\\toolAutoFb\\rs_image\\5.jpg","D:\\Code\\python\\toolAutoFb\\rs_image\\6.jpg"]
+def post_to_group(text, link_groups, driver):
+    for link_group in link_groups:
+        driver.get(link_group)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Bạn viết gì đi...']"))).click()
+        
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]"))).send_keys(text)
+        WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/div[1]/span[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/img[1]"))).click()
+        for image in image_list:
+            
+            driver.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/input[1]").send_keys(image)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Đăng']"))).click()
+        
 
 option = Options()
-
 # Working with the 'add_argument' Method to modify Driver Default Notification
 option.add_argument('--disable-notifications')
 option.add_argument("--lang=en")
 
 # Khởi tạo trình duyệt
-driver = webdriver.Chrome(service=Service(executable_path=r"C:\Users\vytra\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe"), options=option)
+driver = webdriver.Chrome(service=Service(executable_path=r"D:\chromedriver-win64\chromedriver-win64\chromedriver.exe"), options=option)
 
 # Mở trang web của Facebook
 driver.get('https://www.facebook.com')
-
-# Nhập thông tin đăng nhập
-username = 'tranduythuc2003@gmail.com'
-password = '06032307'
 
 username_field = driver.find_element(By.ID, 'email')
 password_field = driver.find_element(By.ID, 'pass')
@@ -31,21 +41,17 @@ password_field = driver.find_element(By.ID, 'pass')
 username_field.send_keys(username)
 password_field.send_keys(password)
 
-# Submit để đăng nhập
+
 password_field.send_keys(Keys.RETURN)
 
-# Chờ trang đăng nhập xong
+
 time.sleep(5)  # Thời gian để trang đăng nhập
 
-# Mở đường link cụ thể tới bài viết
-post_url = 'https://www.facebook.com/nsth.plus/posts/pfbid0bsBMPfHdaa9n1WUhxnEwvXK3osWSddZdYfyf7FQDGgsp7QNkLvbkkN1u7vXcQcwkl'  # Thay thế URL của bài viết cần like vào đây
-driver.get(post_url)
+groups_link = ["https://www.facebook.com/groups/669225251946228","https://www.facebook.com/groups/kiemtienonlinemienphi1", "https://www.facebook.com/groups/kiemtienonline147"
+               ,"https://www.facebook.com/groups/985536349314165", "https://www.facebook.com/groups/359563819672220"]
 
-# Đợi cho trang bài viết được tải hoàn chỉnh
-time.sleep(10)  # Thời gian chờ cho trang bài viết
+message= "Bạn nào đủ 18t ib mình hướng dẫn đăng ký ngân hàng nhận #100k free, rút được hết nhé mn. Sự kiện đến cuối tháng 12 mn tranh thủ nha."
 
-# Thử tìm và ấn nút Like (cần thay đổi theo HTML của trang web)
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Thích']"))).click()
-time.sleep(10)
-# Đóng trình duyệt sau khi hoàn thành
+post_to_group(message, groups_link, driver)
+
 driver.quit()
